1f185335ef35685612af39e7a3afb8ff444f321f
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2021-11-03 08:01:19 +0100

    more helpful comments

--- induction.v
+++ induction.v
@@ -84,6 +84,7 @@ Definition context_decl_map (f:term->term) (c:context_decl) :=
 
 
 Definition createInductionPrinciple (ind_term:term) (ind:one_inductive_body) :=
+    (* auxiliary definitions (mostly for testing purposes) *)
     let PropQ := TemplateToPCUIC.trans [] <% Prop %> in
     let TrueQ := TemplateToPCUIC.trans [] <% True %> in
     let IQ := TemplateToPCUIC.trans [] <% I %> in
@@ -93,6 +94,7 @@ Definition createInductionPrinciple (ind_term:term) (ind:one_inductive_body) :=
         it_mk takes contexts
      *)
 
+     (* separate params, non-uniform params, and indices *)
     let non_uniform_param_count := 0 in
     let ind_type := ind.(ind_type) in (* type of the inductive *)
     let (ctx,retTy) := decompose_prod_assum [] ind_type in (* get params and indices and inner retTy *)
@@ -105,6 +107,13 @@ Definition createInductionPrinciple (ind_term:term) (ind:one_inductive_body) :=
     (* adds quantification of context around body *)
     let quantify ctx body := it_mkProd_or_LetIn ctx body in
 
+    (* construct the quantifications in the predicate
+       these are all non-uniform parameters, the indices, and 
+       an instance of the inductive type instantiated with params and indices
+       
+       the context has to be liftet directly behind the parameter quantification
+       using lift_context [amount] 0 predicate_ctx
+       *)
     let predicate_ctx := 
         [vass (rName "ind_inst") 
             (mkApps ind_term 
@@ -117,11 +126,24 @@ Definition createInductionPrinciple (ind_term:term) (ind:one_inductive_body) :=
         indice_ctx ++ (* quantify over indices *)
         non_uni_param_ctx
     in
+    (* use the context to create the predicate type 
+        ∀ non-uni indices, Ind params non-uni indices -> ℙ
+    *)
     let predicate_type := 
         quantify predicate_ctx
-        PropQ 
+        PropQ (* TODO: correct elimination *)
     in (* type of the predicate used in the induction *)
 
+    (* the conclusion type
+        ∀ non-uni indices (h:Ind params non-uni indices), P non-uni indices h
+        we can see that the first part (up to and including h) is exactly the 
+        predicate context (the quantifications of P)
+        we, therefore, use the context as quantification 
+        (alternative implementation: collect quantifications from type (possibly infered => difficult here) and repeat)
+        (alternative implementation2: use holes and quantify for all arguments (holes are not possible in the type))
+
+        lastly, compute the correct de Bruijn index of P and apply all quantifications
+     *)
     let conclusion_type predicateDist :=
         (quantify
            (lift_context (S predicateDist) 0 predicate_ctx) (* [instance] ++ indice_ctx ++ non_uni_param_ctx *)
@@ -136,6 +158,9 @@ Definition createInductionPrinciple (ind_term:term) (ind:one_inductive_body) :=
         ))
     in
 
+    (*
+    the cases for needed for case analysis/induction
+    *)
     let case_ctx :=
         (* dummy case: forall non-uni indices inst, P *)
         [
@@ -144,38 +169,61 @@ Definition createInductionPrinciple (ind_term:term) (ind:one_inductive_body) :=
         ]
     in
 
+    (*
+    build the context of the type for the induction lemma
+    the context contains the parameters, the predicate, and
+    all cases (possibly extended with induction hypotheses)
 
+    we do not include the conclusion quantifications here as we view them as 
+    a separate section of the induction lemma 
+    (this distinction becomes clear in the proof where we build a fixpoint over the
+    arguments in the conclusion)
+    *)
     let argument_ctx := (* contexts are reversed lists *)
         case_ctx ++
         [vass (rName "P") predicate_type] ++
         param_ctx (* quantify params *)
      in (* all arguments (params, predicate, cases) *)
 
+     (*
+     the type of the induction lemma including the conclusion
+     ∀ params predicate,                                (* preamble *)
+     HCase_1 -> ...                                     (* cases *)
+     HCase_n ->
+     ∀ non-uni indices (h:Ind params non-uni indices),  (* conclusion *)
+       P non-uni indices h
+     *)
     let type := 
         it_mkProd_or_LetIn argument_ctx
-        (* TrueQ *)
         (conclusion_type (#|case_ctx|) ) 
         (* (conclusion_type (1 + #|case_ctx|) )  *)
         (* lift over predicate and cases *)
     in 
 
-    (* allows for holes in the body *)
+    (*
+    build the body (the proof)
+    strictly speaking, we only need the body and need to annotate the type
+    everywhere (which is possible as we have all necessary information)
+
+    but for convenience, we cast the body to the correct type allowing for
+    holes to be filled in during unquoting
+    we will nevertheless annotate the types when easily possible or convenient
+
+    take all arguments from the argument context
+    (params, predicate, cases)
+    and prove the induction lemma using a fixpoint over
+    the conclusion with a case analysis inside
+
+    *)
     tCast 
     (PCUICToTemplate.trans
         (it_mkLambda_or_LetIn
         argument_ctx
         (tRel 0)
-        (* IQ *)
         )
-        (* (tLambda rAnon hole  (* params *)
-        (tLambda (rName "P") hole  (* predicate *)
-        IQ
-        )
-        ) *)
     )
     Cast
     (PCUICToTemplate.trans type).
-    (* ind. *)
     
 
 
@@ -184,8 +232,8 @@ MetaCoq Run (
     (* t <- tmQuote (@Vector.t);; *)
     (* t <- tmQuote (paramTest);; *)
     (* t <- tmQuote (indexTest);; *)
-    t <- tmQuote (depTest);;
-    (* t <- tmQuote (nonUniTest);; *)
+    (* t <- tmQuote (depTest);; *)
+    t <- tmQuote (nonUniTest);;
     match t with
     Ast.tInd {| inductive_mind := k |} _ => 
     ib <- tmQuoteInductive k;;
