2dcbe6126decadb40e9745d16c558e99d192b3c5
Tomas <tomas.vallejos12@gmail.com>
2022-07-11 16:53:21 +0200

    proof manually the normal ind for some translations

--- showcase.v
+++ showcase.v
@@ -12,10 +12,13 @@ Inductive roseA (A:Type) : Type := LeafA (a:A) | NodeA (xs:list2 (roseA A)).
 From MetaCoq.Translations Require Import translation_utils.
 From MetaCoq.Translations Require Import param_original.
 
+Open Scope bs_scope.
 
 MetaCoq Run (TC <- Translate emptyTC "nat" ;;
                 tmDefinition "nat_TC" TC ).
 Print natᵗ.
+MetaCoq Run (createIH false natᵗ).
+Print natᵗ_nested_ind.
 MetaCoq Run (TC <- Translate nat_TC "list" ;;
                 tmDefinition "list_TC" TC ).
 Print listᵗ.
@@ -40,13 +43,57 @@ MetaCoq Run (createIH false roseAᵗ).
 Print roseAᵗ_nested_ind.
 
 
-Lemma nat_natᵗ n: natᵗ n.
-    induction n;constructor;auto.
-Defined.
+Lemma nat_natᵗ_tac : forall n, natᵗ n.   
+Proof. fix f 1. intros n. destruct n.
+    - constructor. 
+    - constructor. apply f.
+Qed.
 
-Lemma list_listᵗ A P: (forall a, P a) -> forall x, listᵗ A P x.
-    induction x;constructor;auto.
-Defined.
+Definition nat_natᵗ_fun : forall n, natᵗ n :=
+    fix f n : natᵗ n :=
+        match n with
+        | O => Oᵗ
+        | S n' => Sᵗ n' (f n')
+        end.
+
+Lemma nat_ind_tac :
+forall P : nat -> Prop,
+P 0 -> (forall n : nat, P n -> P (S n)) -> forall n : nat, P n.
+Proof. intros P P0 PS n; apply natᵗ_nested_ind. 
+    - apply P0. 
+    - apply PS. 
+    - apply nat_natᵗ_tac. 
+Qed.
+
+Definition nat_ind_fun :
+    forall P : nat -> Prop,
+        P 0 -> 
+            (forall n : nat, P n -> P (S n)) ->
+        forall n : nat, P n :=
+    fun P H0 IH n => natᵗ_nested_ind P H0 IH n (nat_natᵗ_fun n).
+
+Lemma list_listᵗ_tac A is_A: (forall a, is_A a) -> forall x, listᵗ A is_A x.
+Proof. intro HA. fix f 1. destruct x.
+    - constructor.
+    - constructor. apply HA. apply f.
+Qed.
+    
+Definition list_listᵗ_fun : forall A is_A, (forall a, is_A a) -> forall x, listᵗ A is_A x:=
+    fix f A is_A AH l : listᵗ A is_A l :=
+    match l with
+    | nil => nilᵗ A is_A
+    | cons h t => consᵗ A is_A h (AH h) t (f A is_A AH t)
+    end.
+
+(* you cannot derive the 'normal' induction principles for container types 
+    using the parametricity translation of that type. Because you need to know the
+    structure of the type that is parameterized (A) to get the proposition is_A
+*)
+(* Lemma list_ind_fun : forall (A : Type) (P : list A -> Prop),
+    P nil ->
+    (forall (a : A) (l : list A), P l -> P (a :: l)%list) -> 
+    forall l : list A, P l :=
+    fun A P P0 IH l => listᵗ_nested_ind A  *)
 
 Lemma list2_list2ᵗ A P: (forall a, P a) -> forall x, list2ᵗ A P x.
     induction x;constructor;auto.
